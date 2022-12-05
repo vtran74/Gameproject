265aa8a1dc8ef59059d3f2049199af49783cec4b
vtran74 <113387275+vtran74@users.noreply.github.com>
2022-12-04 21:59:24 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-//Initializes the game in a local/offline environment
-//This assumes that data/processed-data.js has been generated, using the build.bat script
-//It also requires the data to be rebuilt everytime a change is made in the characters, places or story files.
-function init_local()
+//Initializes the game in a server environment
+//Doesn't rely on the data/processed-data.js file, and asynchronously loads the .json files instead
+//A server environment (or a local *AMP stack, node, python microserver...) is required for the $.getJSON calls to work
+//Except if your browser is particularly chill, in which case, maybe it works okay?
+//I don't know, mine isn't.
+function init_async()
 {
-	initCharacters();
-	parseStory();
-	onInit();
+	$.getJSON("data/characters.json", function(jsonCharacters) {
+		characters = jsonCharacters;
+		initCharacters();
+
+		$.getJSON("data/places.json", function(jsonPlaces) {
+
+			places = jsonPlaces;
+			$.getJSON("data/story.json", function(jsonStory) {
+
+				story = jsonStory;
+				parseStory();
+				onInit();
+			});
+		});
+	});
 }
+
 //Initialize the character data after the data has been loaded
 function initCharacters()
 {
@@ -23,4 +38,18 @@ function onInit()
 	//Only one thing left to do: start the game on the first Scene \o/ Here we go~
 	displayScene("start");
 }
-init_local();
+
+//Are you running the game in a server environment?
+//If you don't, or don't know if you do:
+//- It's okay I still like you;
+//- You will need to double-click build.bat everytime you edit the characters, places or story files;
+//- You'll start the game by double-clicking index.html or game.html
+//- You don't need to touch anything below, you can close this file :)
+
+//If you do:
+//- Great, you can edit the characters, places and story files and see your changes immediately in the game;
+//- Do not start the game by double clicking the html files, use your server path instead;
+//- Comment init_local() and uncomment init_async(), and you're done :)
+
+
+init_async();
