e346c9d5be54f196d5d4b16350211967556c9a90
vtran74 <113387275+vtran74@users.noreply.github.com>
2022-12-05 18:44:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-//Initializes the game in a local/offline environment
-//This assumes that data/processed-data.js has been generated, using the build.bat script
-//It also requires the data to be rebuilt everytime a change is made in the characters, places or story files.
-function init_local()
+function init_async()
 {
-	initCharacters();
-	parseStory();
-	onInit();
+	$.getJSON("data/characters.json", function(jsonCharacters) {
+		characters = jsonCharacters;
+		initCharacters();
+
+		$.getJSON("data/places.json", function(jsonPlaces) {
+
+			places = jsonPlaces;
+			$.getJSON("data/story.json", function(jsonStory) {
+
+				story = jsonStory;
+				parseStory();
+				onInit();
+			});
+		});
+	});
 }
+
 //Initialize the character data after the data has been loaded
 function initCharacters()
 {
@@ -24,16 +34,4 @@ function onInit()
 	displayScene("start");
 }
 
-//Are you running the game in a server environment?
-//If you don't, or don't know if you do:
-//- It's okay I still like you;
-//- You will need to double-click build.bat everytime you edit the characters, places or story files;
-//- You'll start the game by double-clicking index.html or game.html
-//- You don't need to touch anything below, you can close this file :)
-
-//If you do:
-//- Great, you can edit the characters, places and story files and see your changes immediately in the game;
-//- Do not start the game by double clicking the html files, use your server path instead;
-//- Comment init_local() and uncomment init_async(), and you're done :)
-
-init_local();
+init_async();
